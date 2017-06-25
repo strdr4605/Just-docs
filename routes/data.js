9df9303e4431358dafe4f18a8b9f24a10b0627d5
eugenius <eugen.cernei@gmail.com>
2017-06-25 13:10:44 +0300

    backend: Added pdf generation

--- routes/data.js
+++ routes/data.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const pdf = require('./../controllers/pdf_manager.js');
 
 router.post('/', function(req, res, next) {
-  var url = pdf.generate(req.body)
-  res.send(url)
-  res.end()
+  	var url = pdf.generate(req.body)
+  	res.send(url)
+  	res.end()
 });
 
 module.exports = router;
